5e1274bdbfa6758d61b0767e12cce07b3b9ef75a
Ryan Pascoe <ryan.pascoe@outlook.com>
2020-10-12 12:12:43 +0100

    Add isAuthorised variable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 1.1.0 - 2020-10-12
+
+### Added
+
+- Added `isAuthorised` to check if there is an authorised user for the current site.
+
 ## 1.0.0 - 2020-07-28
 
 - Initial release.
--- src/Variable.php
+++ src/Variable.php
@@ -2,6 +2,8 @@
 
 namespace nixondesign\instagram;
 
+use Craft;
+
 /**
  * @author Nixon Design Ltd
  * @since 1.0
@@ -16,5 +18,14 @@ class Variable
     public function getMedia(array $options = [])
     {
         return Plugin::getInstance()->getMedia()->getMedia($options);
+    }
+
+    public function isAuthorised(): bool
+    {
+        $siteId = Craft::$app->getSites()->getCurrentSite()->id;
+
+        $token = Plugin::getInstance()->getAuth()->getTokenBySiteId($siteId);
+
+        return (bool) $token;
     }
 }
